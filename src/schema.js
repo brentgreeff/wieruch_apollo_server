72ef6a907ba551da7f6e7959e63f07d940204b16
Brent <brentgreeff@gmail.com>
2019-06-13 15:24:54 +0200

    Users returned by userByLastName cant be null.

--- src/schema.js
+++ src/schema.js
@@ -5,7 +5,7 @@ import {
 const schema = gql`
   type Query {
     user(id: ID!): User
-    userByLastName(lastName: String!): [User]
+    userByLastName(lastName: String!): [User!]
     users: [User!]
     me: User
 
